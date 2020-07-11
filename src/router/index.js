c217b30522e15a80ee3e564babb299aa8b3041e9
Felipe <felipeas314@gmail.com>
2020-07-11 11:29:58 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Gerentes from '../views/Gerentes.vue'
+import Home from '../views/Home.vue'
+import NovoUsuario from '../views/NovoUsuario.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/gerentes',
+    name: 'gerentes',
+    component: Gerentes
+  },
+  {
+    path:'/cadastrousuario',
+    name:'novo.usuario',
+    component: NovoUsuario
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/NovoUsuario.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <h1>Novo usuário</h1>
+    <form @submit.prevent="sendForm">
+      <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" v-model="usuario.nome">
+      </div>
+      <div class="form-group">
+        <label for="nome">Email</label>
+        <input type="email" class="form-control" v-model="usuario.email">
+      </div>
+      <div class="form-group">
+        <label for="nome">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha">
+      </div>
+      <button class="btn btn-primary" type="submit">Salvar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      usuario : {
+        nome: '',
+        senha: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    sendForm() {
+      axios.post('http://localhost:8000/auth/register',this.usuario)
+        .then(response => console.log('qwer',response))
+        .catch(err => console.log('asdf',err));
+    }
+  }  
+}
+</script>
+
+<style scoped>
+
+</style>
