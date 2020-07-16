1d3cdf9e53245ebf45e7e534cc5f91cdd2d2bcd8
Felipe <felipeas314@gmail.com>
2020-07-16 20:22:39 -0300

    implementação do vuex

---
+++ src/provedor.js
@@ -0,0 +1,46 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import http from '@/http';
+
+Vue.use(Vuex);
+
+const estado = {
+  token: null,
+  usuario: {}
+}
+
+const mutations = {
+  DEFINIR_USUARIO_LOGADO(state, { token, usuario }) {
+      state.token = token
+      state.usuario = usuario
+  },
+  DESLOGAR_USUARIO(state) {
+      state.token = null
+      state.usuario = {}
+  }
+}
+
+const actions = {
+  efetuarLogin({ commit }, usuario) {
+      return new Promise((resolve, reject) => {
+          http.post('auth/login', usuario)
+              .then(response => {
+                  commit('DEFINIR_USUARIO_LOGADO', {
+                      token: response.data.access_token,
+                      usuario: response.data.user
+                  })
+                  resolve(response.data)
+              })
+              .catch(err => {
+                  console.log(err)
+                  reject(err)
+              })
+      })
+  }
+}
+
+export default new Vuex.Store({
+  state: estado,
+  mutations,
+  actions
+})
