b3bd18fcfa4dbd309100433474b75d6cacc327e9
Felipe <felipeas314@gmail.com>
2020-07-16 20:11:34 -0300

    requisição a API

---
+++ src/http/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import provedor from '@provedor'
+
+const http = axios.create({
+  baseURL: 'http://localhost:8000',
+  headers: {
+    'Accept':'application/json',
+    'Content':'application/json'
+  }
+});
+
+http.interceptors.request.use(function(config){
+  const token = provedor.state.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+  },function(erro) {
+    return Promise.reject(erro)
+  });
+
+export default http
